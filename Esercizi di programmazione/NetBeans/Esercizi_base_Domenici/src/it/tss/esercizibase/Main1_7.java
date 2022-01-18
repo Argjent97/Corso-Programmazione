e7683a90fb977cdd212e8f15ba92f82b799ca5be
Argjent97 <argjentkrasniqi97@gmail.com>
2022-01-18 15:59:04 -0600

    Lavoro da casa concluso

--- Esercizi di programmazione/NetBeans/Esercizi_base_Domenici/src/it/tss/esercizibase/Main1_7.java
+++ Esercizi di programmazione/NetBeans/Esercizi_base_Domenici/src/it/tss/esercizibase/Main1_7.java
@@ -4,13 +4,47 @@
  */
 package it.tss.esercizibase;
 
+import java.util.Scanner;
+
 /**
  *
  * @author eigji
  */
 public class Main1_7 {
     public static void main(String[] args){
-        
-    }
-    
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int npari = 0;
+        int ndispari = 0;
+        int mediaP;
+        int mediaD;
+        int sommaP = 0;
+        int sommaD = 0;
+        boolean ContinuaCiclo = true;
+        while(ContinuaCiclo){
+            System.out.println("Inserisci il numero: ");
+            x = sc.nextInt();
+            if(x==0){
+                ContinuaCiclo = false;
+            }
+            else if(x%2 == 0){
+                sommaP = sommaP + x;
+                npari = npari + 1;     
+            }
+            else{
+                sommaD = sommaD + x;
+                ndispari = ndispari + 1;
+            }
+        }
+        mediaP = sommaP / npari;
+        mediaD = sommaD / ndispari;
+        System.out.println("La media dei numeri pari è: " + mediaP);
+        System.out.println("La media dei numeri dispari è: " + mediaD);
+        if(mediaD>mediaP){
+            System.out.println("La media dei numeri dispari è quella maggiore");       
+        }
+        else {
+            System.out.println("La media dei numeri pari è quella maggiore");
+        }       
+    }   
 }
