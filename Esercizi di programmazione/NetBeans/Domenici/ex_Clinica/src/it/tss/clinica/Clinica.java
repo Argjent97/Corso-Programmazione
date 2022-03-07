89f51d607ec72083c22637fc849902c6c7d7db77
Argjent97 <argjentkrasniqi97@gmail.com>
2022-03-07 14:29:16 +0100

    07/03

--- Esercizi di programmazione/NetBeans/Domenici/ex_Clinica/src/it/tss/clinica/Clinica.java
+++ Esercizi di programmazione/NetBeans/Domenici/ex_Clinica/src/it/tss/clinica/Clinica.java
@@ -7,6 +7,7 @@ package it.tss.clinica;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -29,8 +30,12 @@ public class Clinica {
 
     }
 
-    public void cancellaPrenotazione(Specialista specialista, String paziente) {
+    /*public void cancellaPrenotazione(Specialista specialista, String paziente) {
 
+        prenotazioni.removeIf(v -> v.getSpecialista().equals(specialista) && v.getPaziente().equals(paziente));
+    }*/
+    public void cancellaPrenotazione(Specialista specialista, String paziente) {
+        Predicate<Prenotazione> byPaziente = (v-> v.getSpecialista().equals(specialista));
         prenotazioni.removeIf(v -> v.getSpecialista().equals(specialista) && v.getPaziente().equals(paziente));
     }
 
@@ -40,8 +45,15 @@ public class Clinica {
 
         prenotazioni.removeIf(v -> v.equals(p));
     }*/
-    public Collection<String> getPrenotati(Specialista specialista) {
+   /* public Collection<String> getPrenotati(Specialista specialista) {
         List<Prenotazione> tmp = prenotazioni.stream().filter(v -> v.getSpecialista() == specialista).collect(Collectors.toList());
+        Collection<String> ris = tmp.stream().map(v -> v.getPaziente()).collect(Collectors.toList());
+        return ris;
+    }*/
+    
+        public Collection<String> getPrenotati(Specialista specialista) {
+        Predicate<Prenotazione> bySpecialista = (v-> v.getSpecialista().equals(specialista));    
+        List<Prenotazione> tmp = prenotazioni.stream().filter(bySpecialista).collect(Collectors.toList());
         Collection<String> ris = tmp.stream().map(v -> v.getPaziente()).collect(Collectors.toList());
         return ris;
     }
